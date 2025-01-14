569e045ff032354e4476b8b5f08df8ab200f928d
ricp6 <166334380+ricp6@users.noreply.github.com>
2025-01-14 21:49:10 +0000

    limiter api gateway

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,37 +1,35 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
 
-//const limiter = require('./limiter/limiter')
-
 const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
-//app.use(cors({ origin: '*'}));
+// Configuração do limitador
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Limite de 100 requisições por IP
+    message: {
+        error: "Too many requests",
+        details: "You have exceeded the 100 requests in 15 minutes limit!"
+    },
+    standardHeaders: true, // Inclui informações de rate limit nos cabeçalhos `RateLimit-*`
+    legacyHeaders: false,  // Desativa cabeçalhos obsoletos `X-RateLimit-*`
+});
+
+// Aplica o limitador globalmente
+app.use(limiter);
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Limitar meio rudimentar por agora
-// Set up rate limiter: maximum of one hundred requests per minute
-const limiter = RateLimit({
-    windowMs: 10 * 1000, // 10 seconds
-    max: 100,
-});
-// Apply rate limiter to all requests
-app.use(limiter);
-
-// TODO tentar implementar limiter mais complexo
-// Adicionando o middleware ao app
-//app.use(limiter.rateLimiterMiddleware);
 
 app.use('/', indexRouter);
 
