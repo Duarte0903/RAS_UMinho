44ad4085724db92a3e7a8b6f2d0706e293282546
grupo p <grupo-p@gmail.com>
2025-01-20 16:58:58 +0000

    editar e apagar projetos

--- picturas/src/pages/home/home.jsx
+++ picturas/src/pages/home/home.jsx
@@ -6,13 +6,15 @@ import NewProjectDetails from '../../components/new_project_details/new_project_
 import { useSessionStore } from '../../stores/session_store';
 import axios from 'axios';
 import ProjectCard from '../../components/project_card/project_card';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const [show_new_project, set_new_project] = useState(false);
     const {token} = useSessionStore();
     const [userProjects, setUserProjects] = useState([]);
-    const navigate = useNavigate();
+    const [projectId, setProjectId] = useState([]);
+    const [newName, setNewName] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [loading, setLoading] = useState(false); // Para mostrar se está a enviar os dados
 
     const toggleNewProject = () => {
         set_new_project(!show_new_project);
@@ -33,22 +35,110 @@ const Home = () => {
         fetchProjectData();
     }, []);
 
+    const handleEditProject = () => {
+        setIsModalOpen(true);  // Abre o modal para editar o perfil
+    };
+    
+    const handleCloseModal = () => {
+        setIsModalOpen(false);  // Fecha o modal sem fazer nada
+    };
+
+    const onEdit = async () => {
+        setLoading(true); // Define o estado de carregamento para true ao enviar a requisição
+        console.log(`Editar projeto com ID: ${projectId}`);
+
+        try {
+            const response = await axios.put(
+                `https://p.primecog.com/api/users/projects/${projectId}`,
+                { name: newName },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+
+            // Se a resposta for bem-sucedida
+            if (response.status === 200) {
+                alert("Projeto atualizado com sucesso");
+
+                // Fechar o modal após a atualização
+                setIsModalOpen(false);
+            } else {
+                console.error('Erro ao editar o projeto:', response.message || response.error);
+                alert("Erro ao editar o projeto. Tente novamente.");
+            }
+            window.location.reload();
+        } catch (error) {
+            console.error('Erro ao editar o projeto:', error.response?.data || error.message);
+            alert('Erro ao editar projeto.');
+        } finally {
+            setLoading(false); // Finaliza o estado de carregamento
+        }
+    };
+    
+    const onDelete = async (projectId) => {
+        console.log(`Apagar projeto com ID: ${projectId}`);
+        // Implementar a lógica para apagar o projeto
+        const confirmDelete = window.confirm(
+            "Tem certeza de que deseja excluir este projeto? Esta ação não pode ser desfeita."
+        );
+    
+        if (!confirmDelete) return;
+    
+        try {
+            const response = await axios.delete(
+                `https://p.primecog.com/api/users/projects/${projectId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+    
+            if (response.status === 200) {
+                alert("Projeto excluído com sucesso.");
+                window.location.reload();
+            } else {
+                alert("Erro ao excluir o projeto. Tente novamente.");
+            }
+        } catch (error) {
+            console.error("Erro ao excluir projeto:", error);
+            alert("Ocorreu um erro ao tentar excluir o projeto. Por favor, tente novamente mais tarde.");
+        }
+    };
+
     return (
         <div className='home-wrapper'>
             <Navbar />
             <main className='home-container'>
                 <h1>Bem-vindo à PictuRAS !</h1>
 
-                <section className='project-section'>
-                    {userProjects.length > 0 && (
-                            userProjects.map((project) => (
+                <section className="project-section">
+                    {userProjects.length > 0 &&
+                        userProjects.map((project) => (
+                            <div key={project.id} className="project-card-container">
                                 <ProjectCard
-                                    key={project.id}
                                     id={project.id}
                                     projectName={project.name}
                                 />
-                            ))
-                        )}
+                                <div className="project-actions">
+                                    <button 
+                                        className="edit-button" 
+                                        onClick={() => {
+                                                setProjectId(project.id);
+                                                handleEditProject();
+                                            }
+                                        } 
+                                    >
+                                        Editar
+                                    </button>
+                                    <button 
+                                        className="delete-button" 
+                                        onClick={() => onDelete(project.id)}
+                                    >
+                                        Apagar
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    }
                     <NewProject onClick={toggleNewProject} />
                 </section>
 
@@ -56,6 +146,30 @@ const Home = () => {
                     <NewProjectDetails onClose={toggleNewProject} />
                 )}
             </main>
+
+            {/* Modal de edição de projeto */}
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h4>Editar Projeto</h4>
+                        <label htmlFor="new-name">Nome</label>
+                        <input
+                            id="new-name"
+                            type="text"
+                            value={newName}
+                            onChange={(e) => setNewName(e.target.value)}
+                            placeholder="Digite o novo nome do projeto"
+                        />
+
+                        <div className="modal-actions">
+                            <button onClick={onEdit} disabled={loading}>
+                                {loading ? "Aguarde..." : "Salvar"}
+                            </button>
+                            <button onClick={handleCloseModal}>Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
